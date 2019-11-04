53e62b23d951243d7172a6188337d0b1bf3e6835
15872998154 <1978912861@qq.com>
2019-11-04 21:55:56 +0800

    add function

--- todo_list/views.py
+++ todo_list/views.py
@@ -9,16 +9,10 @@ from todo_list.models import List
 def add_item():
     form = ListForm()
     if form.validate_on_submit():
-        # db.create_all()
-        # print("form.title" + form.data['title'])
         transcation = List(title = form.data['title'], description = form.data['description'], status = form.data['tag'], deadline = form.data['deadline'])
-        
         db.session.add(transcation)
-        db.session.commit()
-
-        
+        db.session.commit() 
         return redirect(url_for('list_item'))
-
     return render_template('add_item.html', form = form)
 
 
@@ -39,12 +33,18 @@ def delete_item():
 @app.route('/list_item/update_complete_status', methods=['GET']) #显示待办事项页面
 def update_complete_status():
     item_id = request.args.get("item_id") #获取要修改的事项的ID
-    
     update_item = List.query.filter_by(id = item_id).first() #数据库查询该记录
     update_item.status = 'Complete' #修改内容
     db.session.add(update_item)	    #提交操作
     db.session.commit()
     return redirect(url_for('complete_item'))
+
+@app.route('/list_item/modify_item', methods=['GET']) #显示待办事项页面
+def modify_item():
+    item_id = request.args.get("item_id") #获取要修改的事项的ID
+    
+    modify_item = List.query.filter_by(id = item_id).first() #数据库查询该记录
+    return render_template('modify_item.html', modify_item = modify_item, form = ListForm())    
     
 @app.route('/complete_item', methods=['GET']) #已完成待办事项
 def complete_item():
@@ -55,6 +55,7 @@ def complete_item():
 def uncomplete_item():
     uncomplete_items = List.query.filter(List.status == "Uncomplete").all()
     return render_template('uncomplete_item.html', uncomplete_items = uncomplete_items)
+
 
 
 
